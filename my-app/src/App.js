70596e0d392a365f7c45b5cc0dc6c84bee2fe96a
Warren Smith <warren.lee.smith@hotmail.co.uk>
2024-12-22 20:51:59 +0000

    ADD: Sidebar and CSS

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,27 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
-import FunctionalGreeting from './components/FunctionalGreeting';
-import StatefulGreeting from './components/StatefulGreeting';
+import './components/css/Sidebar.module.css';
 import HooksCounter from './components/HooksCounter';
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <HooksCounter name="Warren" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Sidebar />
     </div>
   );
 }
