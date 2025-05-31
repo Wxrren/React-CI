e35e712041b7bb85103aa8985fbd80d21a67f935
Warren Smith <warren.lee.smith@hotmail.co.uk>
2025-05-31 16:38:27 +0100

    ADD: Loader function - testing

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -1,40 +1,25 @@
 import React, { Component } from "react";
 import css from "./css/Content.module.css";
-import {savedPosts} from "../posts.json";
+import postsData from "../posts.json";
 import PostItem from "./PostItem";
+import Loader from "./Loader";
 
 export class Content extends Component {
     constructor(props) {
         super(props)
+        this.state = { isLoaded: false };
+
     }
     
     render() {
         return (
             <div className={css.Content}>
-                
                 <div className={css.TitleBar}>
                     <h1>My Photos</h1>
                 </div>
-
                 <div className={css.SearchResults}>
-
-                    {/* Part 1: Creating the map function */}
-
-                        {/* {
-                    savedPosts.map((post)=>{
-                            return <div className={css.SearchItem} key={post.title}>
-                                <p>{post.title}</p>
-                                <p>{post.name}</p>
-                                <img src={post.image} alt="random"/>
-                                <p>{post.description}</p>
-                                </div>
-                        })
-                    } */}
-
-
-                    {/* Part 2: Creating a child component */}
-
-                    <PostItem savedPosts={savedPosts} />
+                    <Loader />
+                    <PostItem savedPosts={postsData.savedPosts} />
                 </div>
             </div>
         )
@@ -43,4 +28,4 @@ export class Content extends Component {
 
 export default Content
 
-
+       
---
+++ my-app/src/components/Loader.js
@@ -0,0 +1,12 @@
+import css from "./css/Loader.module.css";
+import React from 'react'
+
+function Loader() {
+  return (
+    <div className= {css.Loader}>
+      
+    </div>
+  )
+}
+
+export default Loader
